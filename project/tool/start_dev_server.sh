1441539eeaa1d5901ee6438d02216fbaaae08eff
yaoyang <kiki@smarty.so>
2020-06-18 15:13:22 +0800

    修改 swoole 配置

--- project/tool/start_dev_server.sh
+++ project/tool/start_dev_server.sh
@@ -11,6 +11,8 @@ sudo docker run --rm -ti -p 80:80 -p 8080:8080 -p 3306:3306 --name swoole_api_fr
     -v $ROOT_DIR/project/config/development/supervisor/queue_worker.conf:/etc/supervisor/conf.d/queue_worker.conf \
     -v $ROOT_DIR/project/config/development/supervisor/swoole_api_frame.conf:/etc/supervisor/conf.d/swoole_api_frame.conf \
     -v $ROOT_DIR/project/config/development/supervisor/swoole_developer.conf:/etc/supervisor/conf.d/swoole_developer.conf \
+    -e 'PRJ_HOME=/var/www/swoole_api_frame' \
+    -e 'ENV=development' \
     -e 'TIMEZONE=Asia/Shanghai' \
     -e 'AFTER_START_SHELL=/var/www/swoole_api_frame/project/tool/after_dev_env_start.sh' \
 kikiyao/debian_php_dev_swoole_env start
