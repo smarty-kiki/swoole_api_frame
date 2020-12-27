5dbc2a8c7f2a23962f2dbcd72153ef6d052d46e6
yaoyang <kiki@smarty.so>
2020-12-27 13:27:07 +0800

    同步 api_frame 修改

--- project/tool/after_dev_env_start.sh
+++ project/tool/after_dev_env_start.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+ROOT_DIR="$(cd "$(dirname $0)" && pwd)"/../../..
+
+mysql -e "create database \`default\`;\
+    GRANT ALL PRIVILEGES ON *.* TO 'default'@'%' IDENTIFIED BY 'password';\
+    GRANT ALL PRIVILEGES ON *.* TO 'default'@'localhost' IDENTIFIED BY 'password';\
+    FLUSH PRIVILEGES"
+
 ENV=development php /var/www/swoole_api_frame/public/cli.php migrate:install
 ENV=development php /var/www/swoole_api_frame/public/cli.php migrate
